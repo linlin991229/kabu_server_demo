1545be59c2a9e4c1c3007b3705064fab427a2205
傲娇小霖霖 <1589861957@qq.com>
2024-03-10 19:04:48 +0800

    配置netty初始化

--- src/main/java/com/lin/kabu_demo/server/channel/BootNettyChannelInitializer.java
+++ src/main/java/com/lin/kabu_demo/server/channel/BootNettyChannelInitializer.java
@@ -16,19 +16,17 @@ import io.netty.handler.codec.string.StringEncoder;
  * @Description
  */
 
-public class BootNettyChannelInitializer<SocketChannel> extends ChannelInitializer<Channel> {
+public class BootNettyChannelInitializer extends ChannelInitializer<Channel> {
 
     @Override
-    protected void initChannel(Channel ch) throws Exception {
+    protected void initChannel(Channel ch) {
 
         // ChannelOutboundHandler，依照逆序执行
         ch.pipeline().addLast("encoder", new StringEncoder());
 
         // 属于ChannelInboundHandler，依照顺序执行
         ch.pipeline().addLast("decoder", new StringDecoder());
-        /**
-         * 自定义ChannelInboundHandlerAdapter
-         */
+        //  自定义ChannelInboundHandlerAdapter
         ch.pipeline().addLast(new BootNettyChannelInboundHandlerAdapter());
 
     }
